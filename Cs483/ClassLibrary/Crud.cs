e3d29f2c92230b722e9fae920f49db408fbdb7fa
Charles P. Gutz <charles.gutz@gmail.com>
2019-10-05 20:28:23 -0300

    WindowsFormApp

---
+++ Cs483/ClassLibrary/Crud.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Crud<T> : ICrud<T>
+    {
+        public List<T> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Listar()
+        {
+            T t = Activator.CreateInstance<T>();
+            List<T> ps = new List<T>();
+            ps.Add(t);
+            return ps;
+        }
+
+        class A
+        {
+            public void CriaLista()
+            {
+                Crud<Pessoa> crudP = new Crud<Pessoa>();
+                var lista = crudP.Listar();
+            }
+        }
+    }
+}
